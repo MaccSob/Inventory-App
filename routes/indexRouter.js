b2fd42bb765a99b26d85fd1d58557efc7886374e
MaccSob <71sobmaciej@gmail.com>
2025-08-22 23:43:50 +0200

    categories tab

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,14 @@ const express = require("express");
 const indexRouter = express.Router();
 
 indexRouter.route("/").get((req, res) => {
-  res.render("index", { title: "Gamerx" });
+  res.render("index", { title: "Home" });
+});
+
+indexRouter.route("/games").get((req, res) => {
+  res.render("games", { title: "Games" });
+});
+indexRouter.route("/categories").get((req, res) => {
+  res.render("categories", { title: "Categories" });
 });
 
 module.exports = indexRouter;
